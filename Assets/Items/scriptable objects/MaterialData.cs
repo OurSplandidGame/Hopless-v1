60b6a59bcb592b38bcd01471c620079fe98296bc
Tianyang Wen <kirawen@umich.edu>
2018-11-18 15:34:52 -0800

    added material item data type

---
+++ Assets/Items/scriptable objects/MaterialData.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New MaterialData", menuName = "Material Data", order = 1)]
+public class MaterialData : ItemData {
+}
